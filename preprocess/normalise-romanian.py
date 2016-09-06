e87db1cdb630c164aaac7aaecbf7e522324b28bc
Rico Sennrich <rico.sennrich@gmx.ch>
2016-09-06 13:47:14 +0100

    work with non-utf8 locales

--- preprocess/normalise-romanian.py
+++ preprocess/normalise-romanian.py
@@ -9,8 +9,9 @@
 import io
 import sys
 istream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+ostream = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 for line in istream:
   line = line.replace("\u015e", "\u0218").replace("\u015f", "\u0219")
   line = line.replace("\u0162", "\u021a").replace("\u0163", "\u021b")
-  print(line, end="")
+  ostream.write(line)
--- preprocess/remove-diacritics.py
+++ preprocess/remove-diacritics.py
@@ -9,6 +9,7 @@
 import io
 import sys
 istream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+ostream = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 for line in istream:
   line = line.replace("\u0218", "S").replace("\u0219", "s") #s-comma
@@ -16,4 +17,4 @@ for line in istream:
   line = line.replace("\u0102", "A").replace("\u0103", "a")
   line = line.replace("\u00C2", "A").replace("\u00E2", "a")
   line = line.replace("\u00CE", "I").replace("\u00EE", "i")
-  print(line, end="")
+  ostream.write(line)
